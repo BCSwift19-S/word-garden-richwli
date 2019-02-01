b3a1a9b82b782a6d42b9dd80e0e3009b55a189f1
BC Swift Student Loan 1 <bcswiftstudentloan1@csomswiftloan1.local>
2019-02-01 13:16:20 -0500

    Section 3.1 & 3.2

--- Word Garden/ViewController.swift
+++ Word Garden/ViewController.swift
@@ -9,12 +9,35 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var headerLabel: UILabel!
+    @IBOutlet weak var guessCountLabel: UILabel!
+    @IBOutlet weak var flowerPicture: UIImageView!
+    @IBOutlet weak var guessFillLabel: UILabel!
+    @IBOutlet weak var userLetterSelectField: UITextField!
+    
+    func updateUIAfterGuess(){
+        userLetterSelectField.resignFirstResponder()
+        userLetterSelectField.text = ""
+         print("UI has been updated.")
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
-
+    
+    @IBAction func guessedLetterFieldChanged(_ sender: UITextField) {
+        print("The guessedLetterFieldChanged has been updated.")
+    }
+    
+    @IBAction func guessThisLetterButton(_ sender: UIButton) {
+        updateUIAfterGuess()
+    }
+    
+    @IBAction func doneKeyPressed(_ sender: UITextField) {
+        updateUIAfterGuess()
+    }
+    
+    @IBAction func playAgainButton(_ sender: UIButton) {
+    }
 }
 
